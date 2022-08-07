1cf43e0b3e973d2765917ddc7125b89fd96ef8d9
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-06 23:22:15 -0300

    implementa authMiddlewares

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,1 +1,37 @@
 import joi from "joi";
+
+export function validateNewUser(req, res, next) {
+    const newUserSchema = joi.object({
+        name: joi.string().pattern(/^[a-zA-Z]*$/).required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        confirmPassword: joi.string().required().valid(joi.ref('password'))
+    });
+    const newUser = req.body;
+    const validation = newUserSchema.validate(newUser, {abortEarly: false});
+
+    if(validation.error) {
+        const errors = validation.error.details.map(each => each.message);
+
+        return res.status(422).send(errors);
+    }
+
+    next();
+}
+
+export function validateUser(req, res, next) {
+    const userSchema = joi.object({
+        name: joi.string().pattern(/^[a-zA-Z]*$/).required(),
+        password: joi.string().required()
+    });
+    const user = req.body;
+    const validation = userSchema.validate(user, {abortEarly: false});
+
+    if(validation.error) {
+        const errors = validation.error.details.map(each => each.message);
+
+        return res.status(422).send(errors);
+    }
+    
+    next();
+}
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signIn, signUp } from '../controllers/authController.js';
+import { validateNewUser, validateUser } from "../middlewares/authMiddlewares.js";
 
 const router = Router();
 
-router.post('/signup', signUp);
-router.post('/signin', signIn);
+router.post('/signup', validateNewUser, signUp);
+router.post('/signin', validateUser, signIn);
 
 export default router;
