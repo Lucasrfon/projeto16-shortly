39da949672dc117cbac5abdd9a00907bb5292839
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-07 04:29:15 -0300

    implementa getUserUrls controller faltando ajustar a resposta

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -17,3 +17,25 @@ export async function ranking(req, res) {
         res.status(500).send(error)
     }
 }
+
+export async function getUserUrls(req, res) {
+    try {
+        const userId = res.locals.user;
+        const {rows: userUrls} = await connection.query(`
+            SELECT users.id, users.name, SUM(visit_count) AS "visitCount"
+            FROM users
+            JOIN urls
+            ON users.id = urls.user_id
+            WHERE users.id = ${userId}
+            GROUP BY users.id
+        `)
+
+        if(!userUrls[0]) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(userUrls[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { ranking } from "../controllers/usersController.js";
+import { getUserUrls, ranking } from "../controllers/usersController.js";
+import { validateToken } from "../middlewares/authMiddlewares.js";
 
 const router = Router();
 
-router.get('/users/me');
+router.get('/users/me', validateToken, getUserUrls);
 router.get('/ranking', ranking);
 
 export default router;
