32590bd8e20a86046a886e9d85b29f4b12c5033a
Lucas Fonseca <lucasrfon@gmail.com>
2022-08-08 14:49:20 -0300

    prepara para deploy

--- src/dbStrategy/pg.js
+++ src/dbStrategy/pg.js
@@ -4,12 +4,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PG_PORT,
-    database: process.env.DATABASE
-  });
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
 
-export { connection }
+const connection = new Pool(databaseConfig);
+
+export default connection;
